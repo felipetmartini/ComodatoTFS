1f81cc99690bac52507ec54650406f14ce272691
felipetmartini <felipemartini@brturbo.com.br>
2014-04-12 21:17:05 -0300

    reverted

--- src/game.cpp
+++ src/game.cpp
@@ -3147,10 +3147,6 @@ void Game::playerLookInBattleList(uint32_t playerId, uint32_t creatureId)
 	} else {
 		lookDistance = -1;
 	}
-	
-	if (!g_events->eventPlayerOnLook(player, creaturePos, creature, 0, lookDistance)) {
-		return;
-	}
 
 	g_events->eventPlayerOnLookInBattleList(player, creature, lookDistance);
 }
