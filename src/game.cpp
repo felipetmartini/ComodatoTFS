389d9749202190b5f126f48f32b5b1226cc92c61
felipetmartini <felipemartini@brturbo.com.br>
2014-04-16 00:07:00 -0300

    Revert "fix test"
    This reverts commit ad56330237453239d31785efdff116b551c4f1c0.

--- src/game.cpp
+++ src/game.cpp
@@ -4587,10 +4587,6 @@ void Game::addCreatureHealth(const SpectatorVec& list, const Creature* target)
 
 void Game::addMagicEffect(const Position& pos, uint8_t effect)
 {
-	if (effect > CONST_ME_LAST) {
-		return;
-	}
-
 	SpectatorVec list;
 	getSpectators(list, pos, true, true);
 	addMagicEffect(list, pos, effect);
@@ -4598,10 +4594,6 @@ void Game::addMagicEffect(const Position& pos, uint8_t effect)
 
 void Game::addMagicEffect(const SpectatorVec& list, const Position& pos, uint8_t effect)
 {
-	if (effect > CONST_ME_LAST) {
-		return;
-	}
-
 	for (Creature* spectator : list) {
 		if (Player* tmpPlayer = spectator->getPlayer()) {
 			tmpPlayer->sendMagicEffect(pos, effect);
@@ -4611,10 +4603,6 @@ void Game::addMagicEffect(const SpectatorVec& list, const Position& pos, uint8_t
 
 void Game::addDistanceEffect(const Position& fromPos, const Position& toPos, uint8_t effect)
 {
-	if (effect > CONST_ANI_LAST || effect == CONST_ANI_UNK1 || effect == CONST_ANI_UNK2 || effect == CONST_ANI_UNK3) {
-		return;
-	}
-
 	SpectatorVec list;
 	getSpectators(list, fromPos, false, true);
 	getSpectators(list, toPos, false, true);
@@ -4623,10 +4611,6 @@ void Game::addDistanceEffect(const Position& fromPos, const Position& toPos, uin
 
 void Game::addDistanceEffect(const SpectatorVec& list, const Position& fromPos, const Position& toPos, uint8_t effect)
 {
-	if (effect > CONST_ANI_LAST || effect == CONST_ANI_UNK1 || effect == CONST_ANI_UNK2 || effect == CONST_ANI_UNK3) {
-		return;
-	}
-
 	for (Creature* spectator : list) {
 		if (Player* tmpPlayer = spectator->getPlayer()) {
 			tmpPlayer->sendDistanceShoot(fromPos, toPos, effect);
