8f5a1b8d9a0acce9111eb45ef478eac71f77c28c
felipetmartini <felipemartini@brturbo.com.br>
2014-04-28 22:56:23 -0300

    Revert "Player:removeExperience function"
    This reverts commit c8aa0439c205528d690468d3d61231fce7a357e7.

--- src/luascript.h
+++ src/luascript.h
@@ -889,7 +889,6 @@ class LuaScriptInterface
 
 		static int32_t luaPlayerGetExperience(lua_State* L);
 		static int32_t luaPlayerAddExperience(lua_State* L);
-		static int32_t luaPlayerRemoveExperience(lua_State* L);
 		static int32_t luaPlayerGetLevel(lua_State* L);
 
 		static int32_t luaPlayerGetMagicLevel(lua_State* L);
